a61be90e5f43dd616f9f34f46d2fbb93baf957da
Kevin Beier <kabeier@gmail.com>
2021-06-17 14:43:31 -0400

    more answers

--- Week_9/Wednesday.py
+++ Week_9/Wednesday.py
@@ -3,8 +3,21 @@
 #  indicating whether there are two numbers in movie_lengths whose sum equals flight_length.
 movie_lengths1=[20, 30, 110, 40, 50] #true
 movie_lengths2=[80, 110, 40]  #false
-
+flight_length=160
+#Set
 def flight_plan_complete (flight_length, movie_lengths):
+    myset=set()
+    for i in movie_lengths:
+        if flight_length-i in myset:
+            return True
+        myset.add(i)
+    return False
+    
+print("output",flight_plan_complete(flight_length, movie_lengths1))
+print("output",flight_plan_complete(flight_length, movie_lengths2))
+
+#Dict
+def flight_plan_complete2 (flight_length, movie_lengths):
     mydict={}
     for i in range(len(movie_lengths)):
         remainder = flight_length - movie_lengths[i]
@@ -12,7 +25,21 @@ def flight_plan_complete (flight_length, movie_lengths):
             return True
         mydict[movie_lengths[i]]=i
     return False
-        
-flight_length=160
-print("output",flight_plan_complete(flight_length, movie_lengths1))
-print("output",flight_plan_complete(flight_length, movie_lengths2))
+
+print("output2",flight_plan_complete2(flight_length, movie_lengths1))
+print("output2",flight_plan_complete2(flight_length, movie_lengths2))    
+
+#TOO SLOW
+def flight_plan_complete3 (flight_length, movie_lengths):
+    for i in movie_lengths:
+        for j in movie_lengths:
+            if i + j == flight_length:
+                return True
+    return False
+
+# Also too slow.  for in with a list is looping over all the values
+def flight_plan_complete4 (flight_length, movie_lengths):
+    for i in movie_lengths:
+        if i - flight_length in movie_lengths:
+            return True
+    return False
